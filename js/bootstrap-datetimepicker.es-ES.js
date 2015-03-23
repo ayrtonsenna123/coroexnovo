66c1746a36c31ea3e1ae1ef6356bf4b36aec5516
Ismael <ismael.martin.martin@gmail.com>
2015-03-23 14:48:13 +0100

    Subida inicial

---
+++ js/bootstrap-datetimepicker.es-ES.js
@@ -0,0 +1,11 @@
+
+;(function($){
+	$.fn.datepicker.dates['es-ES'] = {
+		days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+		daysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom"],
+		daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"],
+		months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+		monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+		today: "Hoy"
+	};
+}(jQuery));
